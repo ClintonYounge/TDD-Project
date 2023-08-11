e5718f478494fcc5610ee84a6f1862fc80fdbe6c
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-12 03:57:29 +0600

    Refactored the tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,113 @@
-require_relative '../main'
+require_relative '../fizzbuzz'  # Adjust the path to your Solver class
 
 RSpec.describe Solver do
   describe '#fizzbuzz' do
     it 'prints the correct fizzbuzz sequence' do
-      solver = Solver.new(100)
+      solver = Solver.new
+      expected_output = <<~OUTPUT
+        1
+        2
+        fizz
+        4
+        buzz
+        fizz
+        7
+        8
+        fizz
+        buzz
+        11
+        fizz
+        13
+        14
+        fizzbuzz
+        16
+        17
+        fizz
+        19
+        buzz
+        fizz
+        22
+        23
+        fizz
+        buzz
+        26
+        fizz
+        28
+        29
+        fizzbuzz
+        31
+        32
+        fizz
+        34
+        buzz
+        fizz
+        37
+        38
+        fizz
+        buzz
+        41
+        fizz
+        43
+        44
+        fizzbuzz
+        46
+        47
+        fizz
+        49
+        buzz
+        fizz
+        52
+        53
+        fizz
+        buzz
+        56
+        fizz
+        58
+        59
+        fizzbuzz
+        61
+        62
+        fizz
+        64
+        buzz
+        fizz
+        67
+        68
+        fizz
+        buzz
+        71
+        fizz
+        73
+        74
+        fizzbuzz
+        76
+        77
+        fizz
+        79
+        buzz
+        fizz
+        82
+        83
+        fizz
+        buzz
+        86
+        fizz
+        88
+        89
+        fizzbuzz
+        91
+        92
+        fizz
+        94
+        buzz
+        fizz
+        97
+        98
+        fizz
+        buzz
+      OUTPUT
 
-      def expected_output(range)
-        range.map do |num|
-          if num % 3 == 0 && num % 5 == 0
-            'fizzbuzz'
-          elsif num % 3 == 0
-            'fizz'
-          elsif num % 5 == 0
-            'buzz'
-          else
-            num
-          end
-        end.join("\n")
-      end
-
-      expect { solver.fizzbuzz }.to output(expected_output(1..100) + "\n").to_stdout
+      expect { solver.fizzbuzz }.to output(expected_output).to_stdout
     end
   end
 end
