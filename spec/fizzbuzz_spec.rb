b11f77748298d55e14f3066be94ed3adf1625f99
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-12 04:13:04 +0600

    Update fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,113 +1,26 @@
-require_relative '../main'  # Adjust the path to your Solver class
+require_relative '../fizzbuzz'  # Adjust the path to your Solver class
 
 RSpec.describe Solver do
   describe '#fizzbuzz' do
     it 'prints the correct fizzbuzz sequence' do
       solver = Solver.new
-      expected_output = <<~OUTPUT
-        1
-        2
-        fizz
-        4
-        buzz
-        fizz
-        7
-        8
-        fizz
-        buzz
-        11
-        fizz
-        13
-        14
-        fizzbuzz
-        16
-        17
-        fizz
-        19
-        buzz
-        fizz
-        22
-        23
-        fizz
-        buzz
-        26
-        fizz
-        28
-        29
-        fizzbuzz
-        31
-        32
-        fizz
-        34
-        buzz
-        fizz
-        37
-        38
-        fizz
-        buzz
-        41
-        fizz
-        43
-        44
-        fizzbuzz
-        46
-        47
-        fizz
-        49
-        buzz
-        fizz
-        52
-        53
-        fizz
-        buzz
-        56
-        fizz
-        58
-        59
-        fizzbuzz
-        61
-        62
-        fizz
-        64
-        buzz
-        fizz
-        67
-        68
-        fizz
-        buzz
-        71
-        fizz
-        73
-        74
-        fizzbuzz
-        76
-        77
-        fizz
-        79
-        buzz
-        fizz
-        82
-        83
-        fizz
-        buzz
-        86
-        fizz
-        88
-        89
-        fizzbuzz
-        91
-        92
-        fizz
-        94
-        buzz
-        fizz
-        97
-        98
-        fizz
-        buzz
-      OUTPUT
 
-      expect { solver.fizzbuzz }.to output(expected_output).to_stdout
+      def expected_output(number)
+        if number % 3 == 0 && number % 5 == 0
+          'fizzbuzz'
+        elsif number % 3 == 0
+          'fizz'
+        elsif number % 5 == 0
+          'buzz'
+        else
+          number.to_s
+        end
+      end
+
+      input_number = 15
+      expected_result = expected_output(input_number) + "\n"
+
+      expect { solver.fizzbuzz(input_number) }.to output(expected_result).to_stdout
     end
   end
 end
