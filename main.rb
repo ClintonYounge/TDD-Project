10e2b939aafbd1d56d342bdd729ded289fe6fc56
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 18:41:05 -0400

    Add methods to main

--- main.rb
+++ main.rb
@@ -1,10 +1,23 @@
 class Solver
-  def factorial
+  def factorial(num)
+    return 1 if num == 0
+    (1..num).inject(:*)
   end
 
-  def reverse
+  def reverse(word)
+    word.reverse
   end
 
-  def fizzbuzz
+  def fizzbuzz(number)
+    num = number
+      if num % 3 == 0 && num % 5 == 0
+        "fizzbuzz"
+      elsif num % 3 == 0
+        "fizz"
+      elsif num % 5 == 0
+        "buzz"
+      else
+        "#{num}"
+      end
   end
 end
